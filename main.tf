d0d986cbd3808b86a9923c23c49bc42fce5e26cd
Denislav Denov <denislav@hashicorp.com>
2024-04-23 13:28:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,21 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  shared_config_files = [var.tfc_aws_dynamic_credentials.default.shared_config_file]
 }
+
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    })
+    aliases = map(object({
+      shared_config_file = string
+    }))
+  })
+}
+
 
 # Create a VPC
 resource "aws_vpc" "example" {
